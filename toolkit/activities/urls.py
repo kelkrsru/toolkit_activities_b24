b887d91f38ab5eb7a62f0ad4b57a2a20d34fcff4
kelkrsru <kelkrsru@gmail.com>
2022-09-14 14:12:48 +0700

    initial commit

--- toolkit/activities/urls.py
+++ toolkit/activities/urls.py
@@ -7,4 +7,5 @@ app_name = 'activities'
 urlpatterns = [
     path('install/', views.install, name='install'),
     path('uninstall/', views.uninstall, name='uninstall'),
+    path('operator-add/', views.operator_add, name='operator_add'),
 ]
--- toolkit/activities/views.py
+++ toolkit/activities/views.py
@@ -1,11 +1,17 @@
+import json
+from http import HTTPStatus
+
+from django.core.exceptions import ObjectDoesNotExist
+
 from core.bitrix24.bitrix24 import ActivityB24
 from core.models import Portals
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from pybitrix24 import Bitrix24
 
 from activities.models import Activity
+from settings.models import SettingsPortal
 
 
 @csrf_exempt
@@ -49,7 +55,77 @@ def uninstall(request):
     return JsonResponse({'result': result})
 
 
-def response_for_bp(portal, event_token, log_message, return_values=None):
+@csrf_exempt
+def operator_add(request):
+    """View for activity copy products."""
+    with open('/root/test.log', 'w', encoding='utf-8') as file:
+        file.write(json.dumps(request.POST))
+    # initial_data: dict[str, any] = _get_initial_data(request)
+    # portal, settings_portal = _create_portal(initial_data)
+    # smart_element_id, deal_id = _check_initial_data(portal, initial_data)
+    # smart_process_code = _initial_smart_process(portal, initial_data)
+    # smart_process = SmartProcessB24(portal, 0)
+    # products = smart_process.get_all_products(smart_process_code,
+    #                                           smart_element_id)
+    # deal = DealB24(portal, deal_id)
+    # keys_for_del = ['id', 'ownerId', 'ownerType']
+    # for product in products:
+    #     for key in keys_for_del:
+    #         del product[key]
+    # deal.get_all_products()
+    # products += deal.products
+    # deal.set_products(products)
+    # _response_for_bp(
+    #     portal,
+    #     initial_data['event_token'],
+    #     'Успех. Товары скопированы.',
+    #     return_values={'result': f'Ok: {products = }'},
+    # )
+    return HttpResponse(status=HTTPStatus.OK)
+
+
+def _create_portal(initial_data):
+    """Method for create portal."""
+    try:
+        portal = Portals.objects.get(member_id=initial_data['member_id'])
+        portal.check_auth()
+        settings_portal = SettingsPortal.objects.get(portal=portal)
+        return portal, settings_portal
+    except ObjectDoesNotExist:
+        return HttpResponse(status=HTTPStatus.BAD_REQUEST)
+
+
+def _get_initial_data(request):
+    """Method for get initial data from Post request."""
+    if request.method != 'POST':
+        return HttpResponse(status=HTTPStatus.BAD_REQUEST)
+    return {
+        'member_id': request.POST.get('auth[member_id]'),
+        'event_token': request.POST.get('event_token'),
+        'smart_element_id': request.POST.get(
+            'properties[smart_element_id]') or 0,
+        'deal_id': request.POST.get('properties[deal_id]') or 0,
+        'document_type': request.POST.get('document_type[2]'),
+    }
+
+
+def _check_initial_data(portal, initial_data):
+    """Method for check initial data."""
+    try:
+        smart_element_id = int(initial_data['smart_element_id'])
+        deal_id = int(initial_data['deal_id'])
+        return smart_element_id, deal_id
+    except Exception as ex:
+        _response_for_bp(
+            portal,
+            initial_data['event_token'],
+            'Ошибка. Проверьте входные данные.',
+            return_values={'result': f'Error: {ex.args[0]}'},
+        )
+        return HttpResponse(status=HTTPStatus.OK)
+
+
+def _response_for_bp(portal, event_token, log_message, return_values=None):
     """Method for send parameters in bp."""
     bx24 = Bitrix24(portal.name)
     bx24._access_token = portal.auth_id
